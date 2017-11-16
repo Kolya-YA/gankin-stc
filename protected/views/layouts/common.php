993b574740fc40aab73efb55f4dc43d6b8c4f2cb
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2017-11-16 03:02:07 +0100

    change css import order

--- protected/views/layouts/common.php
+++ protected/views/layouts/common.php
@@ -23,20 +23,6 @@
 	<script src="/js/script.js"></script>
 	<script src="/js/jquery.datepicker.min.js"></script>
 	<script src="/js/common.js"></script>
-	
-	<!--[if lt IE 8]>
-	<div style=' clear: both; text-align:center; position: relative;'>
-		<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
-		<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
-		</a>
-	</div>
-	<![endif]-->
-	<!--[if lt IE 9]>
-	<link href='http://fonts.googleapis.com/css?family=Orbitron:400' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Orbitron:700' rel='stylesheet' type='text/css'>
-	<script src="/js/html5.js"></script>
-	<link rel="stylesheet" href="/css/ie.css"> 
-	<![endif]-->
 
   <?php
     $LastModified_unix = strtotime(date("D, d M Y H:i:s", filectime($_SERVER['SCRIPT_FILENAME'])));
