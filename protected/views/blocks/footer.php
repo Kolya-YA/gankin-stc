1953830d9d6aadbc7a829b0e1bcd28bb53df4bea
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2018-02-26 13:52:37 +0100

    update footer css & html

--- protected/views/blocks/footer.php
+++ protected/views/blocks/footer.php
@@ -19,14 +19,14 @@
 
         <symbol id="instagram-icon" viewBox="0 0 94 94">
             <path d="M64.748,32.155h8.781c1.923,0,3.496-1.574,3.496-3.498v-8.371c0-1.925-1.573-3.498-3.496-3.498h-8.781
-			c-1.924,0-3.497,1.573-3.497,3.498v8.371C61.251,30.581,62.824,32.155,64.748,32.155z"/>
-            <ellipse cx="47.074" cy="46.831" rx="15.602" ry="15.117"/>
+			c-1.924,0-3.497,1.573-3.497,3.498v8.371C61.251,30.581,62.824,32.155,64.748,32.155z"></path>
+            <ellipse cx="47.074" cy="46.831" rx="15.602" ry="15.117"></ellipse>
             <path d="M89,0H5C2.239,0,0,2.239,0,5v84c0,2.761,2.239,5,5,5h84c2.762,0,5-2.239,5-5V5C94,2.239,91.762,0,89,0z M85.843,75.872
 			c0,5.911-4.487,9.971-9.971,9.971H18.128c-5.484,0-9.971-4.06-9.971-9.971V18.128c0-5.911,4.486-9.971,9.971-9.971h57.744
-			c5.483,0,9.971,4.06,9.971,9.971V75.872z"/>
+			c5.483,0,9.971,4.06,9.971,9.971V75.872z"></path>
             <path d="M71.221,47.676c0,12.922-10.812,23.396-24.146,23.396c-13.336,0-24.147-10.475-24.147-23.396
 			c0-2.316,0.35-4.553,0.997-6.666h-7.136v32.815c0,1.698,1.391,3.088,3.089,3.088h54.099c1.697,0,3.088-1.39,3.088-3.088V41.01
-			h-6.838C70.871,43.123,71.221,45.359,71.221,47.676z"/>
+			h-6.838C70.871,43.123,71.221,45.359,71.221,47.676z"></path>
         </symbol>
 
         <symbol id="google-plus-icon" viewBox="0 0 94 94">
@@ -36,12 +36,12 @@
 		c-10.634,0-16.654-7.95-16.654-15.792c0-4.62,2.104-9.752,6.418-13.473c5.724-4.726,13.541-6.008,18.966-6.008h19.771l-6.525,4.099
 		h-6.217c2.307,1.909,7.117,5.931,7.117,13.578c0,7.436-4.209,10.964-8.421,14.28c-1.306,1.304-2.813,2.712-2.813,4.929
 		c0,2.207,1.508,3.413,2.608,4.325l3.617,2.809c4.414,3.72,8.428,7.138,8.428,14.083C57.482,76.005,48.346,85.553,31.088,85.553z
-		 M84.482,45.77H74.395v10.117H69.43V45.77H59.391v-5.02H69.43V30.695h4.965V40.75h10.088V45.77z"/>
+		 M84.482,45.77H74.395v10.117H69.43V45.77H59.391v-5.02H69.43V30.695h4.965V40.75h10.088V45.77z"></path>
             <path d="M45.24,30.749c0-7.243-4.317-18.503-12.64-18.503c-2.616,0-5.42,1.307-7.029,3.32c-1.707,2.114-2.204,4.821-2.204,7.443
-		c0,6.734,3.911,17.894,12.544,17.894c2.507,0,5.207-1.2,6.821-2.811C45.04,35.771,45.24,32.56,45.24,30.749z"/>
+		c0,6.734,3.911,17.894,12.544,17.894c2.507,0,5.207-1.2,6.821-2.811C45.04,35.771,45.24,32.56,45.24,30.749z"></path>
             <path d="M39.82,57.195c-0.8-0.102-1.302-0.102-2.305-0.102c-0.904,0-6.324,0.197-10.537,1.615
 		c-2.208,0.799-8.627,3.214-8.627,10.354c0,7.141,6.922,12.276,17.658,12.276c9.635,0,14.755-4.637,14.755-10.862
-		C50.764,65.334,47.451,62.627,39.82,57.195z"/>
+		C50.764,65.334,47.451,62.627,39.82,57.195z"></path>
         </symbol>
 
         <symbol id="twitter-icon" viewBox="0 0 94 94">
@@ -56,7 +56,7 @@
 		c-0.208-1.04-0.312-2.11-0.312-3.191c0-8.936,7.271-16.205,16.206-16.205c4.396,0,8.639,1.806,11.681,4.96
 		c3.432-0.699,6.697-1.96,9.715-3.75c0.139-0.082,0.312-0.069,0.438,0.03c0.125,0.101,0.176,0.268,0.126,0.421
 		c-1.021,3.199-3.018,5.989-5.696,8.004c2.523-0.439,4.974-1.182,7.302-2.214c0.157-0.072,0.342-0.027,0.453,0.104
-		C85.959,22.97,85.971,23.158,85.874,23.302z"/>
+		C85.959,22.97,85.971,23.158,85.874,23.302z"></path>
         </symbol>
 
     </svg>
@@ -65,88 +65,93 @@
 
         <div class="page-footer__flex-wrapper">
 
-            <div class="page-footer__block">
-                <div class="footer-about">
-                    <a href="#" class="footer-about__home-link">
-                        <p class="footer-about__title">surf-Tarifa.com</p>
-                        <!--                    <img src="#" alt="Surf-tarifa logo" class="footer-about__logo" width="180" height="80">-->
+            <div class="page-footer__about">
+
+                <a href="#" class="footer-about__home-link">
+                    <p class="footer-about__title">surf-Tarifa.com</p>
+                    <!--                    <img src="#" alt="Surf-tarifa logo" class="footer-about__logo" width="180" height="80">-->
+                </a>
+
+                <div class="footer-about__social">
+
+                    <a href="fb" class="footer-about__social-item">
+                        <div class="footer-about__icon-container">
+                            <svg class="footer-about__icon">
+                                <use xlink:href="#facebook-icon"></use>
+                            </svg>
+                        </div>
+                        <p>Facebook</p>
                     </a>
 
-                    <div class="footer-about__social">
+                    <a href="i" class="footer-about__social-item">
+                        <div class="footer-about__icon-container">
+                            <svg class="footer-about__icon">
+                                <use xlink:href="#instagram-icon"></use>
+                            </svg>
+                        </div>
+                        <p>Instagram</p>
+                    </a>
 
-                        <a href="fb" class="footer-about__social-item">
-                            <div class="footer-about__icon-container">
-                                <svg class="footer-about__icon">
-                                    <use xlink:href="#facebook-icon"></use>
-                                </svg>
-                            </div>
-                            <p>Facebook</p>
-                        </a>
+                    <a href="gplus" class="footer-about__social-item">
+                        <div class="footer-about__icon-container">
+                            <svg class="footer-about__icon">
+                                <use xlink:href="#google-plus-icon"></use>
+                            </svg>
+                        </div>
+                        <p>Google+</p>
+                    </a>
 
-                        <a href="i" class="footer-about__social-item">
-                            <div class="footer-about__icon-container">
-                                <svg class="footer-about__icon">
-                                    <use xlink:href="#instagram-icon"></use>
-                                </svg>
-                            </div>
-                            <p>Instagram</p>
-                        </a>
+                    <a href="tw" class="footer-about__social-item">
+                        <div class="footer-about__icon-container">
+                            <svg class="footer-about__icon">
+                                <use xlink:href="#twitter-icon"></use>
+                            </svg>
+                        </div>
+                        <p>Twitter</p>
+                    </a>
 
-                        <a href="gplus" class="footer-about__social-item">
-                            <div class="footer-about__icon-container">
-                                <svg class="footer-about__icon">
-                                    <use xlink:href="#google-plus-icon"></use>
-                                </svg>
-                            </div>
-                            <p>Google+</p>
-                        </a>
+                </div>
 
-                        <a href="tw" class="footer-about__social-item">
-                            <div class="footer-about__icon-container">
-                                <svg class="footer-about__icon">
-                                    <use xlink:href="#twitter-icon"></use>
-                                </svg>
-                            </div>
-                            <p>Twitter</p>
-                        </a>
+                <p class="footer-about__address"><?= Yii::t('app', 'address') ?></p>
+                <p class="footer-about__phone"><?= Yii::t('app', 'phone') ?></p>
 
+            </div>
+
+            <div class="page-footer__nav-wrapper">
+
+                <div class="page-footer__block">
+                    <div class="footer-nav">
+                        <a href="/equipment"><?= Yii::t('app', 'renteq') ?></a>
+                        <a href="/school"><?= Yii::t('app', 'findsurf') ?></a>
                     </div>
-
-                    <p class="footer-about__address"><?= Yii::t('app', 'address') ?></p>
-                    <p class="footer-about__phone"><?= Yii::t('app', 'phone') ?></p>
-
                 </div>
+
+                <div class="page-footer__block">
+                    <div class="footer-nav">
+                        <? if (Yii::app()->user->isGuest): ?>
+
+                            <a href="<?= $this->createUrl('site/login') ?>"><?= Yii::t('menu', 'login') ?></a>
+                            <a href="<?= $this->createUrl('site/register') ?>"><?= Yii::t('menu', 'register') ?></a>
+                        <? else : ?>
+                            <a href="<?= $this->createUrl('site/userpanel') ?>"><?= Yii::t('menu', 'userpanel') ?></a>
+                            <a href="<?= $this->createUrl('site/logout') ?>"><?= Yii::t('menu', 'logout') ?></a>
+                        <? endif ?>
+                        <a href="/news"><?= Yii::t('app', 'news') ?></a>
+                        <a href="/faq"><?= Yii::t('app', 'faq') ?></a>
+                        <a href="/partner"><?= Yii::t('app', 'become_partner') ?></a>
+                    </div>
+                </div>
+
+                <div class="page-footer__block">
+                    <div class="footer-nav">
+                        <a href="/rental"><?= Yii::t('app', 'rental_policies') ?></a>
+                        <a href="/privacy"><?= Yii::t('app', 'privacy_policy') ?></a>
+                        <a href="/impressum"><?= Yii::t('app', 'impressum') ?></a>
+                    </div>
+                </div>
+
             </div>
 
-            <div class="page-footer__block">
-                <div class="footer-nav">
-                    <a href="/equipment"><?= Yii::t('app', 'renteq') ?></a>
-                    <a href="/school"><?= Yii::t('app', 'findsurf') ?></a>
-                </div>
-            </div>
-
-            <div class="page-footer__block">
-                <div class="footer-nav">
-                    <? if (Yii::app()->user->isGuest): ?>
-
-                        <a href="<?= $this->createUrl('site/login') ?>"><?= Yii::t('menu', 'login') ?></a>
-                        <a href="<?= $this->createUrl('site/register') ?>"><?= Yii::t('menu', 'register') ?></a>
-                    <? else : ?>
-                        <a href="<?= $this->createUrl('site/userpanel') ?>"><?= Yii::t('menu', 'userpanel') ?></a>
-                        <a href="<?= $this->createUrl('site/logout') ?>"><?= Yii::t('menu', 'logout') ?></a>
-                    <? endif ?>
-                    <a href="/faq"><?= Yii::t('app', 'faq') ?></a>
-                    <a href="/partner"><?= Yii::t('app', 'become_partner') ?></a>
-                </div>
-            </div>
-
-            <div class="page-footer__block">
-                <div class="footer-nav">
-                    <a href="/rental"><?= Yii::t('app', 'rental_policies') ?></a>
-                    <a href="/privacy"><?= Yii::t('app', 'privacy_policy') ?></a>
-                    <a href="/impressum"><?= Yii::t('app', 'impressum') ?></a>
-                </div>
-            </div>
         </div>
 
         <div class="page-footer__copyright">
